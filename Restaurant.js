4fe7a61985d95a533d5ac37b749cdb4646650b31
ShqipdonDrenica <63073692+ShqipdonDrenica@users.noreply.github.com>
2020-05-21 01:09:39 +0200

    Add files via upload

--- Restaurant.js
+++ Restaurant.js
@@ -6,14 +6,38 @@
         var username = document.getElementById('username');
         var password = document.getElementById('password');
 
-        if(username.value.trim() ==""  ){
+        if(username.value.trim() == ""){
             alert('Please fill your username');
             username.focus();
         
-        }else if(password.value.trim() =="" ){
+        }else if(password.value.trim() ==""){
                 alert('Please fill your password ')
                 password.focus();
         }
         
     }
-    
+    function send() {
+        var yourName = document.getElementById('yourName');
+        var yourEmail=document.getElementById('yourEmail');
+        var subject = document.getElementById('subject');
+        var yourMessage=document.getElementById('yourMessage');
+
+        if(yourName.value.trim()== ""){
+            alert('Please fill your name');
+            yourName.focus();
+            
+        }else if(yourEmail.value.trim()== ""){
+            alert('Please fill your email');
+            yourEmail.focus();
+
+        }else if(subject.value.trim()== ""){
+            alert('Please fill your subject');
+            subject.focus();
+
+        }else if(yourMessage.value.trim()== ""){
+            alert('Please fill your message');
+            yourMessage.focus();
+        }
+        location.reload();
+        
+    }
