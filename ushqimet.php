6c4b7903b8a0d41cbf554caa7b07817824547375
haxhillolluni <63076274+haxhillolluni@users.noreply.github.com>
2020-07-11 20:38:54 +0200

    Add files via upload

---
+++ ushqimet.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+        <title>Contact US</title>
+        <link href="food.css" rel="stylesheet" type="text/css" /></title> 
+         
+        <div class="container" style="height: 60px;">
+            <a href="Restaurant.php">
+            <img src="logo.jpg" alt="Restaurant.php" style="padding-left: 20px;" width="55px" height="55px" />
+            </a>
+            
+            <div id="list">
+                <ul>
+                    <li class="lista"><a href="Restaurant.php">Home</a> </li>
+                    <li class="lista"><a href="About_US.php">About Us</a> </li>
+                    <li class="lista"><a href="Contact.php">Contact</a></li> 
+                </ul>
+               
+            </div>
+         </div>
+    </head>
+<body>
+    <div class = 'header'>
+        <h1>Full Menu</h1>
+        <input id="myinput" type="text" placeholder="Search...">
+    </div>
+
+    <div class  = "show">
+        <ul class = 'main'>
+            <li class="solid">
+                <img class="logo" src="436145260z.jpg" alt = 'biftek'></img>
+                <p class = "para">biftek</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="lunch-dinner_soups-salads_crispy-chicken-salad.jpg" alt = 'sallate pule'></img>
+                <p class = "para">sallate pule</p>
+            </li>
+            <li class="solid">
+                <img class ="logo" src="Feature-restaurant-butcher-bakery-shops2.jpg" alt = 'sandwich'></img>
+                <p class = "para">sandwich</p>
+            </li>
+            <li class="solid">
+                <img class ="logo" src="Pizza-Margarita.png" alt = 'pice margarita'></img>
+                <p class = "para">pica margarita</p>
+            </li>
+        </ul>
+        <ul class = 'main'>
+            <li class="solid">
+                <img class="logo" src="fasule-1200x630.jpg" alt = 'pasul ne tave'></img>
+                <p class = "para">pasul ne tave</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="hqdefault.jpg" alt = 'gullash me mish viqi'></img>
+                <p class = "para">gullash me mish viqi</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="download.jpg" alt = 'qofte ne tave'></img>
+                <p class = "para">qofte ne tave</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="omeelperi-780x439.jpg" alt = 'omlet me proshute'></img>
+                <p class = "para">omlet me proshute</p>
+            </li>
+        </ul>
+        <ul class = 'main'>
+            <li class="solid">
+                <img class="logo" src="download (1).jpg" alt = 'filete pule'></img>
+                <p class="para">filete pule</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="featured-great-american-hamburger.jpg" alt = 'hamburger'></img>
+                <p class = "para">hamburger</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="hqdefault (1).jpg" alt = 'doner'></img>
+                <p class = "para">doner</p>
+            </li>
+            <li class="solid">
+                <img class="logo" src="download (2).jpg" alt = 'qebapa'></img>
+                <p class = "para">qebapa</p>
+            </li>
+        </ul>
+        <ul class=" main">
+            <li class="solid">
+                <img class="logo" src="download (1).jpg" alt = 'filete pule'></img>
+                <p class = "para">filete pule</p>
+            </li>
+        </ul>
+    </div>
+
+    <div class="show">
+        <input name = "fname" id = "myinput" type=" text" value = ''>
+        <input name = "fname" id = "myinput" type=" text" value="">
+        <button type = 'button' onclick="myFunction()">Post</button>
+    </div>
+    
+    <script ></script>
+</body>
+</html>
