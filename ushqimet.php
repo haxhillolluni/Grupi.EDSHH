9281173706c2c340de94c77bde85b35eb67b21d8
haxhillolluni <63076274+haxhillolluni@users.noreply.github.com>
2020-07-24 10:46:04 +0200

    Add files via upload

--- ushqimet.php
+++ ushqimet.php
@@ -2,29 +2,28 @@
 <html lang="en">
 <head>
     <head>
-        <title>Contact US</title>
-        <link href="food.css" rel="stylesheet" type="text/css" /></title> 
-         
+        <title>Ushqimet</title>
+        <link href="css/food.css" rel="stylesheet" type="text/css" /></title>
         <div class="container" style="height: 60px;">
-            <a href="Index.php">
-            <img src="logo.jpg" alt="Index.php" style="padding-left: 20px;" width="55px" height="55px" />
+            <a href="Restaurant.php">
+            <img src="logo.jpg" alt="Restaurant.php" style="padding-left: 20px;" width="55px" height="55px" />
             </a>
             
             <div id="list">
                 <ul>
-                    <li class="lista"><a href="Index.php">Home</a> </li>
+                    <li class="lista"><a href="Restaurant.php">Home</a> </li>
+                    <li class="lista"><a href="ushqimet.php">Ushqimet</a></li>
                     <li class="lista"><a href="About_US.php">About Us</a> </li>
-                    <li class="lista"><a href="Contact.php">Contact</a></li> 
+                    <li class="lista"><a href="Contact.php">Contact</a></li>
+                    
                     <li class="lista"><a href="Historiku.php">Historiku</a></li>
                 </ul>
-               
             </div>
          </div>
     </head>
 <body>
     <div class = 'header'>
         <h1>Full Menu</h1>
-        <input id="myinput" type="text" placeholder="Search...">
     </div>
 
     <div class  = "show">
@@ -82,20 +81,84 @@
                 <p class = "para">qebapa</p>
             </li>
         </ul>
-        <ul class=" main">
-            <li class="solid">
-                <img class="logo" src="download (1).jpg" alt = 'filete pule'></img>
-                <p class = "para">filete pule</p>
-            </li>
-        </ul>
     </div>
+    <?php
+        $serverName = 'DESKTOP-J435FOV';
+        $connectionInfo = array( "Database"=>"Webi", "UID"=>"DesktopUser", "PWD"=>"12345678");
+        $conn = sqlsrv_connect( $serverName, $connectionInfo );
+        if( $conn === false ) {
+            die( print_r( sqlsrv_errors(), true));
+        }
+       
+        $sql = "SELECT * FROM Post";
+        $stmt = sqlsrv_query( $conn, $sql );
+        if( $stmt === false) {
+            die( print_r( sqlsrv_errors(), true) );
+       }
+       
+        $vlera = 0;
+        while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {      
+            if($vlera == 4 || $vlera == 0){
+                $vlera = 0;
+    ?>
+                <ul class=" main">
+    <?php
+            }
+            ?>   
+                <li class="solid">
+                    <img class="logo" src="<?php echo $row[1];?>" alt = 'filete pule'></img>
+                    <p class = "para">filete pule</p>
+                </li>            
+            <?php
+            if($vlera == 3){
+            ?>
+                </ul>
+            <?php
+            }
+                $vlera++;
+            
+        }
+        
+        sqlsrv_free_stmt( $stmt);
+    ?>
 
-    <div class="show">
+   <form  action = "views/insertPosts.php" method = "post">
         <input name = "fname" id = "myinput" type=" text" value = ''>
-        <input name = "fname" id = "myinput" type=" text" value="">
-        <button type = 'button' onclick="myFunction()">Post</button>
-    </div>
-    
-    <script  src="food.js" type="text/javascript" language="javscript"></script>
+        <input name = "fname1" id = "myinput" type=" text" value="">
+        <input type = 'submit'  value = "Post" submit="false" name="Submitbtn">
+    </form>
+    <script>
+            function myFunction(){
+                var post = {
+                    s : '',
+                    p : '' 
+                };
+                s = document.getElementsByName('fname')[0].value;
+                p = document.getElementsByName('fname')[1].value;
+
+                var img = document.createElement('img');
+                img.src = s;
+                img.className += "logo";
+                var createdP = document.createElement('p');
+                createdP.className += "para";
+                createdP.innerHTML = p;
+                var createdli = document.createElement('li');
+                createdli.className += "solid";
+                createdli.appendChild(img);
+                createdli.appendChild(createdP);
+                var x = document.getElementsByClassName('main');
+                var y = x[x.length - 1];
+                listElements = y.getElementsByTagName('li');
+                var len = listElements.length;
+                if(len < 4){
+                    y.appendChild(createdli);
+                }else{
+                    var createdUl = document.createElement('ul');
+                    createdUl.className += "main";
+                    document.getElementsByClassName('show')[0].appendChild(createdUl);
+                    createdUl.appendChild(createdli);
+                }
+            }
+    </script>
 </body>
 </html>
